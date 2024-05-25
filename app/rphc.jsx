91211e53c13f831a48c54bdf290dd15616cd170b
Yunus Parvez Khan <developer.ypk@gmail.com>
2024-05-26 01:47:32 +0530

    chore: change content in pages

--- app/rphc.jsx
+++ app/rphc.jsx
@@ -6,7 +6,8 @@ const Rphc = () => {
     return (
         <View className="flex items-center flex-1 justify-center text-gray-100 bg-gray-900" >
             <View className="items-center m-10" >
-                <Text className="text-gray-100 text-8xl" >RPHC</Text>
+                <Text className="text-gray-100 text-6xl text-center mt-4 mb-2" >Rampurhat</Text>
+                <Text className="text-gray-100 text-6xl text-center mb-4" >College</Text>
                 <Text className="text-gray-100 text-2xl" >Rampurhat, West Bengal</Text>
             </View>
             <Link href="/" className='py-3 px-8 bg-gray-800 text-gray-200 text-xl'>Go Back</Link>
--- app/snhu.jsx
+++ app/snhu.jsx
@@ -6,8 +6,9 @@ const Snhu = () => {
     return (
         <View className="flex items-center flex-1 justify-center text-gray-100 bg-gray-900" >
             <View className="items-center m-10" >
-                <Text className="text-gray-100 text-8xl" >SNHU</Text>
-                <Text className="text-gray-100 text-2xl" >Manchester, New Hempshire</Text>
+                <Text className="text-gray-100 text-5xl text-center mt-4" >Southern</Text>
+                <Text className="text-gray-100 text-5xl text-center mb-4" >New Hampshire University</Text>
+                < Text className="text-gray-100 text-2xl" >Manchester, New Hempshire</Text>
             </View>
             <Link href="/" className='py-3 px-8 bg-gray-800 text-gray-200 text-xl'>Go Back</Link>
         </View>
