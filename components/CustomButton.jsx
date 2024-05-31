23840fb2c7ceaedd0deccfd7edb4fe8fa63457b6
Yunus Parvez Khan <developer.ypk@gmail.com>
2024-06-01 01:32:56 +0530

    feat(comp): create directory for components and a custom button comp

---
+++ components/CustomButton.jsx
@@ -0,0 +1,33 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            className={`
+                bg-yellow-500 
+                rounded-xl 
+                min-h-[62px] 
+                justify-center items-center 
+                ${containerStyles}
+                ${isLoading && 'opacity-50'}
+            `}
+            disabled={isLoading}
+        >
+            <Text
+                className={`
+                    text-gray-900 
+                    font-semibold 
+                    text-lg 
+                    ${textStyles}
+                `}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+export default CustomButton
