013eb1f13eb6956242070b0c1d9e3184d2f38294
Yunus Parvez Khan <developer.ypk@gmail.com>
2024-06-01 21:14:52 +0530

    refactor(App): use custom tailwind theme classNames

--- app/index.jsx
+++ app/index.jsx
@@ -8,7 +8,7 @@ import { StatusBar } from 'expo-status-bar'
 
 const App = () => {
     return (
-        <SafeAreaView className="h-full bg-[#161622]" >
+        <SafeAreaView className="h-full bg-primary" >
             <ScrollView contentContainerStyle={{ height: '100%', justifyContent: 'center' }} className="w-full" >
                 <View className="w-full items-center min-h-[85vh] px-4" >
                     <Image source={images.logo} className="w-[130px] h-[84]px" resizeMode='contain' />
@@ -35,8 +35,8 @@ const App = () => {
                     </View>
                     <Text className="text-gray-100 text-sm font-pregular mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
                     <CustomButton
-                        title="Continue"
-                        handlePress={() => router.push('/home')}
+                        title="Sign In"
+                        handlePress={() => router.push('/sign-in')}
                         containerStyles="w-full mt-7"
                     />
                 </View>
