0a9c14e02a16e8fc6dad3a68fffc516de759798c
Yunus Parvez Khan <developer.ypk@gmail.com>
2024-06-01 23:19:59 +0530

    refactor(components): remove poppins font from `FormField`

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -7,7 +7,7 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
     const [showPassword, setShowPassword] = useState(false);
     return (
         <View className={`space-y-2 ${otherStyles}`}>
-            <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+            <Text className='text-base text-gray-100 font-medium'>{title}</Text>
             <View
                 className='
                     w-full h-16 px-4 
@@ -20,7 +20,7 @@ const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, .
                     '
             >
                 <TextInput
-                    className='flex-1 text-white font-psemibold text-base w-full'
+                    className='flex-1 text-white font-semibold text-base w-full'
                     style={{ outline: 'none' }}
                     value={value}
                     placeholder={placeholder}
